9bf817ad6a02b39565d65b77b55f5ea19b2a657a
Ramasare Yadav <yadavrama096@gmail.com>
2026-08-11 14:12:48 +0530

    team working traking numnber

---
+++ src/main/java/org/example/B/Test123.java
@@ -0,0 +1,9 @@
+package org.example.B;
+
+public class Test123 {
+    static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
